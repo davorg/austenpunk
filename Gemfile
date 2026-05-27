29421a37bb19f2e54f8944dba0e9e83c432c73e7
Dave Cross <dave@davecross.co.uk>
2026-05-27 16:06:50 +0100

    Initial version

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+source "https://rubygems.org"
+
+gem "jekyll"
+gem "webrick"
+gem "jekyll-sitemap"
---
+++ _plugins/book_pages.rb
@@ -0,0 +1,25 @@
+class BookPage < Jekyll::Page
+  def initialize(site, book)
+    @site = site
+    @base = site.source
+    @dir  = "books/#{book["slug"]}"
+    @name = "index.html"
+
+    process(@name)
+    read_yaml(File.join(@base, "_layouts"), "book.html")
+
+    data["book"] = book
+    data["title"] = book["title"]
+    data["description"] = book["teaser"] || book["blurb"]
+  end
+end
+
+class BookPageGenerator < Jekyll::Generator
+  safe true
+
+  def generate(site)
+    site.data["books"].each do |book|
+      site.pages << BookPage.new(site, book)
+    end
+  end
+end
